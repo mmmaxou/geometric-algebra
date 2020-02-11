8a051e3d98b01e21c974ce4dcef58502428c041d
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2020-02-11 09:52:33 +0100

    Rotation and scale is ok

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 // Create a Clifford Algebra with 3,0,1 metric.
 const objs = Algebra(3, 0, 1, () => {
 
+  // scale
+  const scale = 0.1
+
+  // Degree
+  const degree2radian = (deg) => (deg * (3.15149) / 360)
+
   // rotation helper and Lathe function.
   const point = (x, y, z) => 1e123 - x * 1e012 + y * 1e013 + z * 1e023
+  const line = (px, py, pz, dx, dy, dz) => px * 1e01 + py & 1e02 + pz * 1e03 + dx * 1e12 + dy * 1e13 + dz * 1e23
   const rot = (a, P) => Math.cos(a) + Math.sin(a) * P.Normalized
   const lathe = (X, n, P, m) => [...Array(n + 1)].map((x, i) => rot(i / n * Math.PI * (m || 1), P) >>> X)
 
@@ -21,49 +28,39 @@ const objs = Algebra(3, 0, 1, () => {
 
   // Basic primitives constructed by Lathing points, line segments, etc.
   const cylinder = (r = 1, h = 1, x = 32) => wrap(lathe([!1e0, !(1e0 + r * 1e3), !(1e0 + r * 1e3 + h * 1e1), !(1e0 + h * 1e1)], x, 1e23))
-  const torus = (r = .3, r2 = .25, x = 32, y = 16) => wrap(lathe((1 + r * .5e03) >>> lathe(!(1e0 + r2 * (1e1 + 1e3) / 2 ** .5), y, 1e13), x, 1e23))
-  const sphere = (r = 1, x = 32, y = 16) => wrap(lathe(lathe(!(1e0 + r * 1e1), y, 1e13, .5), x, 1e23))
-  const cone = (r = 1, h = 1, x = 64) => wrap(lathe([!1e0, !(1e0 + r * 1e3), !(1e0 + h * 1e1)], x, 1e23))
-  const arrow = () => [...cone(.15, .3), ...cone(.15, 0), ...cylinder(.05, -2)]
+  const cube = (r = 1) => cylinder(r * scale, r * scale * 1.33, 4)
 
   // A selection of these objects.
   const objs = [
-    arrow(),
-    torus(0.8, .3),
-    sphere(.8),
-    sphere(.8, 3, 2),
-    cone(1, 2 ** .5, 3),
-    cone(1, 2 ** .5, 4),
-    cone(1, 2 ** .5),
-    torus(.8, .2, 5, 32),
-    cylinder(),
-    cylinder(1, 2 ** .5, 4),
-    torus(.8, .3, 4, 4),
-    torus(.8, .3, 64, 4)
-  ].map(x => ({ data: x }));
+    cube(1),
+    cube(1),
+    cube(1),
+  ].map((x, i) => ({ data: x, selected: false, rot: { x: degree2radian(45), y: 0, z: 0 } }));
 
+  // Camera
   const camera = 0e0
+
 
   // Render and rotate them using the webGL2 previewer.
   const displayer = document.getElementById("displayer")
   displayer.appendChild(this.graph(() => {
 
     // Get the time
-    const time = performance.now() / 1000;
-    camera.set(Math.cos(time) + Math.sin(time) * 1e13)
-
+    const time = performance.now() / 4000;
     const res = []
 
     // Transform all objects
     objs.forEach((obj, i) => {
-      const scale = 1.5
-      const rotY = rot(time, 1e12)
-      const rotZ = rot(time * 0.331, 1e13)
+      camera.set(Math.cos(time) + Math.sin(time) * 1e13);
+      const rotation = obj.rot || { x: 0, y: 0, z: 0 }
+      const rotX = rot(rotation.x, 1e23)
+      const rotY = rot(rotation.y, 1e12)
+      const rotZ = rot(rotation.z, 1e13)
       const X_Move = 1e03 * scale
-      const Y_Move = 3e01 * scale
+      const Y_Move = 0e01 * scale
       const Z_Move = 1e02 * scale
       const translate = (1 + 2e0 - Y_Move - ((i % 3) - 1) * X_Move - ((i / 3 | 0) - 1.5) * Z_Move)
-      obj.transform = (translate * rotZ * rotY).Scale(0.1)
+      obj.transform = (translate * rotZ * rotY * rotX)
       res.push(obj.selected ? 0x11FF88 : 0xFF0088)
       res.push(obj)
     });
@@ -74,7 +71,7 @@ const objs = Algebra(3, 0, 1, () => {
   }, { gl: 1, animate: true, camera }));
 
 
-  const center = (n, size) => (n - (size / 2)) / size
+  const center = (n, size) => (n - (size * 0.5)) * 2 / size
 
   // Add a on click button
   displayer.addEventListener('click', (e) => {
@@ -86,10 +83,18 @@ const objs = Algebra(3, 0, 1, () => {
     console.log(objs)
 
     const p1 = point(x, y, 0);
+    const p2 = point(x, y, -10)
+    const ray = line(p1.e012, p1.e013, p1.e023, p2.e012, p2.e013, p2.e023)
     objs.push(p1)
+    objs.push(p2)
+    objs.push(ray)
 
     // Test for intersection
     objs[4].selected = objs[4].selected ? false : true
+
+
+    const time = performance.now() / 4000;
+    console.log('time is ', time)
 
   })
 
